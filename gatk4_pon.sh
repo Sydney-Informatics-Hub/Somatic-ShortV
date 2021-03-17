7ddc772524036fd9f11827a0c9024a4b694fbcbd
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-18 09:30:53 +1100

    Add header and remove module load

--- gatk4_pon.sh
+++ gatk4_pon.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-# Run GATK Mutect2 (create PON) using scatter-gather method
-# Runs Mutect2 in tumour only mode on normal samples
-
-module load gatk/4.1.2.0
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Runs GATK4 Mutect2 on normal samples for
+# creating a panel of normal (PoN)
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 18/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 ref=`echo $1 | cut -d ',' -f 1`
 sample=`echo $1 | cut -d ',' -f 2`
