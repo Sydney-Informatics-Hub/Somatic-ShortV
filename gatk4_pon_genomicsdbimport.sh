1bcd283ded51695b0564a9681f2a8f245f4d425f
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-18 17:31:36 +1100

    Add header, move module loads, remove java -Xms

--- gatk4_pon_genomicsdbimport.sh
+++ gatk4_pon_genomicsdbimport.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-# Consolidate interval VCFs across multiple samples for GenotypeGVCFs (joint-calling)
-
-module load gatk/4.1.2.0
-module load samtools/1.10
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Consolidate per sample VCFs into a single database
+# for multi-sample processing using GenomicsDBImport
+# creating a panel of normal (PoN)
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 18/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 ref=`echo $1 | cut -d ',' -f 1`
 cohort=`echo $1 | cut -d ',' -f 2`
@@ -34,20 +56,13 @@ echo "$(date) : Start GATK 4 GenomicsDBImport. Reference: ${ref}; Cohort: ${coho
 #mkdir -p ${out}
 #cd ${out}
 
-gatk --java-options "-Xmx64g -Xms64g" \
-	GenomicsDBImport \
-	--sample-name-map ${sample_map} \
-	--overwrite-existing-genomicsdb-workspace \
-	--genomicsdb-workspace-path ${out} \
-	--tmp-dir ${tmp} \
-	--reader-threads ${nt} \
-	--intervals ${interval} >>${logdir}/${index}.oe 2>&1
+gatk --java-options "-Xmx64g" \
+        GenomicsDBImport \
+        --sample-name-map ${sample_map} \
+        --overwrite-existing-genomicsdb-workspace \
+        --genomicsdb-workspace-path ${out} \
+        --tmp-dir ${tmp} \
+        --reader-threads ${nt} \
+        --intervals ${interval} >>${logdir}/${index}.oe 2>&1
 
 echo "$(date): Finished GATK 4 consolidate VCFs with GenomicsDBImport for: ${out}" >>${logdir}/${index}.oe 2>&1
-
-#Caveats
-#IMPORTANT: The -Xmx value the tool is run with should be less than the total amount of physical memory available by at least a few GB, as the native TileDB library #requires additional memory on top of the Java memory. Failure to leave enough memory for the native code can result in confusing error messages!
-#At least one interval must be provided
-#Input GVCFs cannot contain multiple entries for a single genomic position
-#The --genomicsdb-workspace-path must point to a non-existent or empty directory.
-#GenomicsDBImport uses temporary disk storage during import. The amount of temporary disk storage required can exceed the space available, especially when specifying a #large number of intervals. The command line argument `--tmp-dir` can be used to specify an alternate temporary storage location with sufficient space..
