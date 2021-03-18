0d5fe5d00887700dbd2c0671d29fffa14dc8df77
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-18 11:32:28 +1100

    Add header, move module loads

--- gatk4_pon_gathervcfs.sh
+++ gatk4_pon_gathervcfs.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-# Merge interval level Mutect2 (PoN) VCFs per sample
-# Final output is gzipped and tabix indexed
-
-module load gatk/4.1.2.0
-module load samtools/1.10
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Gather interval PoN VCFs into per sample,
+# gzipped VCFs that are tabix indexed
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 18/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 set -e
 
@@ -17,7 +37,6 @@ mkdir -p ${logdir}
 rm -rf ${logdir}/${sample}.oe
 
 gatk GatherVcfs \
-	--arguments_file ${args} \
-	--MAX_RECORDS_IN_RAM 1000000 \
-	-O ${out} >> ${logdir}/${sample}.oe 2>&1
-
+        --arguments_file ${args} \
+        --MAX_RECORDS_IN_RAM 1000000 \
+        -O ${out} >> ${logdir}/${sample}.oe 2>&1
