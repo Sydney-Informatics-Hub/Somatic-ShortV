8a459dd90b9503cccb5711ed16c34cae368088c5
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-19 16:46:53 +1100

    Update gatk4_mutect2_check_pair_parallel.sh

--- gatk4_mutect2_check_pair_parallel.sh
+++ gatk4_mutect2_check_pair_parallel.sh
@@ -1,15 +1,42 @@
 #! /bin/bash
 
-# Check gatk4_mutect2 tumour-normal pair calling
-# Write inputs for failed tasks/missing interval VCFs per sample
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Check gatk4_mutect2 job, writes inputs for failed tasks
+# Runs gatk4_mutect2_check_pair.sh for each tumour-normal pair in parallel
+# Maximum number of samples processed in parallel is 48
+# Usage: nohup sh gatk4_mutect2_check_pair_parallel.sh /path/to/cohort.config 2> /dev/null &
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 18/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
 
 if [ -z "$1" ]
 then
-	echo "Please run this script with the base name of your config file, e.g. sh gatk4_hc_make_input.sh samples_batch1"
-	exit
+        echo "Please run this script with the base name of your config file, e.g. sh gatk4_hc_make_input.sh samples_batch1"
+        exit
 fi
 
-cohort=$1
+config=$1
+cohort=$(basename $config | cut -d'.' -f 1)
 config=../$cohort.config
 SCRIPT=./gatk4_mutect2_check_pair.sh
 INPUTS=./Inputs
@@ -20,40 +47,38 @@ rm -rf ${inputfile}
 # Collect sample IDs from samples.config
 # Only collect IDs from normal samples (labids ending in -B)
 while read -r sampleid labid seq_center library; do
-	if [[ ! ${sampleid} =~ ^#.*$ && ${labid} =~ -B.?$ || ${labid} =~ -N.?$ ]]; then
-		samples+=("${labid}")
-	fi
+        if [[ ! ${sampleid} =~ ^#.*$ && ${labid} =~ -B.?$ || ${labid} =~ -N.?$ ]]; then
+                samples+=("${labid}")
+        fi
 done < "${config}"
 
 # Collect all tumour IDs for each normal sample
 for nmid in "${samples[@]}"; do
-	# Find any matching tumour bams using normal id without -N or -B
-	patient=$(echo "${nmid}" | perl -pe 's/(-B.?|-N.?)$//g')
-	patient_samples=( $(awk -v pattern="${patient}-" '$2 ~ pattern{print $2}' ${config}) )
-	if (( ${#patient_samples[@]} == 2 )); then
-		nmid=`printf '%s\n' ${patient_samples[@]} | grep -P 'N.?$|B.?$'`
-		tmid=`printf '%s\n' ${patient_samples[@]} | grep -vP 'N.?$|B.?$'`
-		inputs+=("${cohort},${tmid},${nmid}")
-	elif (( ${#patient_samples[@]} > 2 )); then
-		nmid=`printf '%s\n' ${patient_samples[@]} | grep -P 'N.?$|B.?$'`
-		for sample in "${patient_samples[@]}"; do
-			if ! [[ ${sample} =~ -N.?$ || ${sample} =~ -B.?$ ]]; then
-				tmid=${sample}
-				inputs+=("${cohort},${tmid},${nmid}")
-			fi
-		done
-	fi
+        # Find any matching tumour bams using normal id without -N or -B
+        patient=$(echo "${nmid}" | perl -pe 's/(-B.?|-N.?)$//g')
+        patient_samples=( $(awk -v pattern="${patient}-" '$2 ~ pattern{print $2}' ${config}) )
+        if (( ${#patient_samples[@]} == 2 )); then
+                nmid=`printf '%s\n' ${patient_samples[@]} | grep -P 'N.?$|B.?$'`
+                tmid=`printf '%s\n' ${patient_samples[@]} | grep -vP 'N.?$|B.?$'`
+                inputs+=("${cohort},${tmid},${nmid}")
+        elif (( ${#patient_samples[@]} > 2 )); then
+                nmid=`printf '%s\n' ${patient_samples[@]} | grep -P 'N.?$|B.?$'`
+                for sample in "${patient_samples[@]}"; do
+                        if ! [[ ${sample} =~ -N.?$ || ${sample} =~ -B.?$ ]]; then
+                                tmid=${sample}
+                                inputs+=("${cohort},${tmid},${nmid}")
+                        fi
+                done
+        fi
 done
 
-#echo "${samples[@]}"
-#echo "${samples[@]}" | xargs --max-args 1 --max-procs 2 ${SCRIPT}
 echo "$(date): Checking .vcf.gz, .vcf.gz.tbi, vcf.gz.stats, f1r2 files for ${#inputs[@]} tumour normal pairs"
 
-echo "${inputs[@]}" | xargs --max-args 1 --max-procs 20 ${SCRIPT}
+echo "${inputs[@]}" | xargs --max-args 1 --max-procs 48 ${SCRIPT}
 
 if [[ -s ${inputfile} ]]; then
-	num_inputs=`wc -l ${inputfile}`
-	echo "$(date): There are ${num_inputs} tasks to run for gatk4_mutect2_missing_run_parallel.pbs"
+        num_inputs=`wc -l ${inputfile}`
+        echo "$(date): There are ${num_inputs} tasks to run for gatk4_mutect2_missing_run_parallel.pbs"
 else
-	echo "$(date): There are 0 tasks to run for gatk4_mutect2_missing_run_parallel.pbs"
+        echo "$(date): There are 0 tasks to run for gatk4_mutect2_missing_run_parallel.pbs"
 fi
