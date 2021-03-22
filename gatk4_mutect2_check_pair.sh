3487c4373500e0b2a0e5a885adfe2ab5598956df
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-23 09:47:39 +1100

    Update gatk4_mutect2_check_pair.sh

--- gatk4_mutect2_check_pair.sh
+++ gatk4_mutect2_check_pair.sh
@@ -7,7 +7,7 @@
 # Run by gatk4_mutect2_check_pair_parallel.sh
 # Author: Tracy Chew
 # tracy.chew@sydney.edu.au
-# Date last modified: 18/03/2021
+# Date last modified: 23/03/2021
 #
 # If you use this script towards a publication, please acknowledge the
 # Sydney Informatics Hub (or co-authorship, where appropriate).
@@ -37,55 +37,65 @@ tmpfile=`echo $1 | cut -d ',' -f 8`
 scatterlist=`echo $1 | cut -d ',' -f 9`
 bamdir=`echo $1 | cut -d ',' -f 10`
 scatterdir=`echo $1 | cut -d ',' -f 11`
+logdir=`echo $1 | cut -d ',' -f 12`
+pair=${tmid}_${nmid}
 nmbam=${bamdir}/${nmid}.final.bam
 tmbam=${bamdir}/${tmid}.final.bam
-logs=./Logs/gatk4_mutect2/${tmid}_${nmid}
 nt=1
 num_intervals=$(wc -l $scatterlist | cut -d' ' -f 1)
+PERL_SCRIPT=get_interval_times_gatklog.pl
 
 i=0
 echo "$(date): Checking tumour ${tmid} normal ${nmid} pair..."
 for interval in $(seq -f "%04g" 0 $((${num_intervals}-1))); do
-        logfile=${logs}/${interval}.oe
-        vcf=${outdir}/${tmid}_${nmid}.unfiltered.${interval}.vcf.gz
-        tbi=${outdir}/${tmid}_${nmid}.unfiltered.${interval}.vcf.gz.tbi
-        stats=${outdir}/${tmid}_${nmid}.unfiltered.${interval}.vcf.gz.stats
-        f1r2=${outdir}/${tmid}_${nmid}.f1r2.${interval}.tar.gz
+        logfile=${logdir}/${pair}/${interval}.oe
+        vcf=${outdir}/${pair}.unfiltered.${interval}.vcf.gz
+        tbi=${outdir}/${pair}.unfiltered.${interval}.vcf.gz.tbi
+        stats=${outdir}/${pair}.unfiltered.${interval}.vcf.gz.stats
+        f1r2=${outdir}/${pair}.f1r2.${interval}.tar.gz
         if ! [[ -s "${vcf}" &&  -s "${tbi}" && -s "${stats}" && -s "${f1r2}" && -s "${logfile}" ]]
         then
                 ((++i))
                 intfile=$(grep ${interval} ${scatterlist})
-                echo "${ref},${tmid},${tmbam},${nmid},${nmbam},${scatterdir}/${intfile},${pon},${germline},${outdir},${logs},${nt}" >> ${tmpfile}
+                echo "${ref},${tmid},${tmbam},${nmid},${nmbam},${scatterdir}/${intfile},${pon},${germline},${outdir},${logdir}/${pair},${nt}" >> ${tmpfile}
         else
                 success=$(grep -i success ${logfile})
                 error=$(grep -i error ${logfile})
                 if [[ ! ${success} && -z ${error} ]]; then
                         ((++i))
                         intfile=$(grep ${interval} ${scatterlist})
-                        echo "${ref},${tmid},${tmbam},${nmid},${nmbam},${scatterdir}/${intfile},${pon},${germline},${outdir},${logs},${nt}" >> ${tmpfile}
+                        echo "${ref},${tmid},${tmbam},${nmid},${nmbam},${scatterdir}/${intfile},${pon},${germline},${outdir},${logdir}/${pair},${nt}" >> ${tmpfile}
                 fi
         fi
 done
 
 # Check chrM
-chrM_logfile=${logs}/chrM.oe
-chrM_vcf=${outdir}/${tmid}_${nmid}.unfiltered.chrM.vcf.gz
-chrM_tbi=${outdir}/${tmid}_${nmid}.unfiltered.chrM.vcf.gz.tbi
-chrM_stats=${outdir}/${tmid}_${nmid}.unfiltered.chrM.vcf.gz.stats
-chrM_f1r2=${outdir}/${tmid}_${nmid}.f1r2.chrM.tar.gz
+chrM_logfile=${logdir}/${pair}/chrM.oe
+chrM_vcf=${outdir}/${pair}.unfiltered.chrM.vcf.gz
+chrM_tbi=${outdir}/${pair}.unfiltered.chrM.vcf.gz.tbi
+chrM_stats=${outdir}/${pair}.unfiltered.chrM.vcf.gz.stats
+chrM_f1r2=${outdir}/${pair}.f1r2.chrM.tar.gz
 
 if ! [[ -s "${chrM_vcf}" &&  -s "${chrM_tbi}" && -s "${chrM_stats}" && -s "${chrM_f1r2}" && -s "${chrM_logfile}" ]]
 then
         ((++i))
-        echo "${ref},${tmid},${tmbam},${nmid},${nmbam},chrM,${pon},${germline},${outdir},${logs},${nt}" >> ${tmpfile}
+        echo "${ref},${tmid},${tmbam},${nmid},${nmbam},chrM,${pon},${germline},${outdir},${logdir}/${pair},${nt}" >> ${tmpfile}
 else
         success=$(grep -i success ${chrM_logfile})
         error=$(grep -i error ${chrM_logfile})
         if [[ ! ${success} && -z ${error} ]]; then
                 ((++i))
-                echo "${ref},${tmid},${tmbam},${nmid},${nmbam},chrM,${pon},${germline},${outdir},${logs},${nt}" >> ${tmpfile}
+                echo "${ref},${tmid},${tmbam},${nmid},${nmbam},chrM,${pon},${germline},${outdir},${logdir}/${pair},${nt}" >> ${tmpfile}
         fi
 fi
 
-echo "$(date): Tumour ${tmid} normal ${nmid} pair has $i failed tasks."
-
+if [[ ${i}>0 ]]; then
+        echo "$(date): ${pair} has $i failed tasks."
+else
+        echo "$(date): ${pair} has $i failed tasks. Printing task duration and memory usage..."
+        perl ${PERL_SCRIPT} ${logdir}/${pair} > ${logdir}/${pair}/${pair}_task_duration_mem.txt
+        echo "$(date): Tarring ${pair} logs..."
+        cd ${logdir}
+        tar -czf ${pair}_mutect2_logs.tar.gz ${pair}
+        rm -rf ${logdir}
+fi
