04904cee597befec2f9c93f2c22e8218ffc63324
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-23 12:30:39 +1100

    Update gatk4_cohort_pon_check.sh

--- gatk4_cohort_pon_check.sh
+++ gatk4_cohort_pon_check.sh
@@ -1,18 +1,40 @@
 #! /bin/bash
 
-# Create input file to check log files in parallel
-# For each sample, record minutes taken per interval
-# Flag any intervals with error messages
-# If there are no error messages, archive log files in a per sample tarball
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Check gatk4_cohort_pon job, write input for failed tasks
+# Usage: nohup sh gatk4_cohort_pon_check.sh /path/to/cohort.config 2> /dev/null &
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 18/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
 
 if [ -z "$1" ]
 then
-	echo "Please run this script with the base name of your config file, e.g. sh gatk4_genomicsdbimport_missing_make_input.sh samples_batch1"
-	exit
+        echo "Please provide the path to your cohort.config file, e.g nohup sh gatk4_cohort_pon_check.sh /path/to/cohort.config 2> /dev/null &"
+        exit
 fi
 
-cohort=$1
-config=../$cohort.config
+config=$1
+cohort=$(basename $config | cut -d'.' -f 1)
 INPUTS=./Inputs
 inputfile=${INPUTS}/gatk4_cohort_pon_missing.inputs
 ref=../Reference/hs38DH.fasta
@@ -22,7 +44,9 @@ scatterlist=$scatterdir/3200_ordered_exclusions.list
 gendbdir=./$cohort\_PoN_GenomicsDBImport
 outdir=./$cohort\_cohort_PoN
 logdir=./Logs/gatk4_cohort_pon
+PERL_SCRIPT=./gatk4_check_logs.pl
 perlfile=${logdir}/interval_duration_memory.txt
+num_int=`wc -l ${scatterlist} | cut -d' ' -f 1`
 
 # Increase CPU as necessary
 
@@ -32,31 +56,46 @@ mkdir -p ${logdir}
 rm -rf ${inputfile}
 rm -rf ${perlfile}
 
-# Run perl script to get duration
-echo "$(date): Checking log files for errors, obtaining duration and memory usage per task..."
-perl gatk4_check_logs.pl "$logdir"
+# Check each interval vcf and tbi file exists and is not empty
+for index in $(seq -f "%04g" 0 $((${num_int}-1))); do
+        vcf=${outdir}/${cohort}.${index}.pon.vcf.gz
+        tbi=${outdir}/${cohort}.${index}.pon.vcf.gz.tbi
+        if ! [[ -s "${vcf}" && -s "${tbi}" ]]
+        then
+                redo+=("$index")
+        fi
+done
+echo $(date): "${#redo[@]}" tasks had missing or empty VCF or TBI files, wrote to $inputfile.
 
-# Check output file 
+# Check log files. Run perl script to get duration
+echo "$(date): Checking log files for errors, obtaining duration and memory usage per task..."
+perl $PERL_SCRIPT "$logdir"
+
+# Check output file
 while read -r interval duration memory; do
-	if [[ $duration =~ NA || $memory =~ NA ]]
-	then
-		redo+=("$interval")
-	fi
+        if [[ $duration =~ NA || $memory =~ NA ]]
+        then
+                if [[ ! "${redo[@]}" =~ "${interval}" ]]
+                then
+                        redo+=("$interval")
+                fi
+        fi
 done < "$perlfile"
 
 if [[ ${#redo[@]}>1 ]]
 then
-	echo "$(date): There are ${#redo[@]} intervals that need to be re-run."
-	echo "$(date): Writing inputs to ${inputfile}"
 
-	for redo_interval in ${redo[@]}; do
-        	interval="${scatterdir}/${redo_interval}-scattered.interval_list"
-		echo "${ref},${cohort},${gnomad},${gendbdir},${interval},${outdir},${logdir}" >> ${inputfile}
-	done
+        echo "$(date): There are ${#redo[@]} intervals that need to be re-run."
+        echo "$(date): Writing inputs to ${inputfile}"
+
+        for redo_interval in ${redo[@]}; do
+                interval="${scatterdir}/${redo_interval}-scattered.interval_list"
+                echo "${ref},${cohort},${gnomad},${gendbdir},${interval},${outdir},${logdir}" >> ${inputfile}
+        done
 else
-	echo "$(date): There are no intervals that need to be re-run. Tidying up..."
-	cd ${logdir}
-	tar --remove-files \
-		-kczf cohort_pon_logs.tar.gz \
-		*.oe
+        echo "$(date): There are no intervals that need to be re-run. Tidying up..."
+        cd ${logdir}
+        tar --remove-files \
+        -kczf cohort_pon_logs.tar.gz \
+        *.oe
 fi
