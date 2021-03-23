5d745b510801b7623680b843b0afcdef821b5812
tracyc <49257820+tracychew@users.noreply.github.com>
2021-03-23 12:23:40 +1100

    Update gatk4_mutect2.sh

--- gatk4_mutect2.sh
+++ gatk4_mutect2.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-# Run GATK Mutect2 using scatter-gather method
-
-module load gatk/4.1.2.0
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Runs GATK4 Mutect2 on tumour-normal pairs
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 23/03/2021
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Bioinformatics
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
 
 ref=`echo $1 | cut -d ',' -f 1`
 tmid=`echo $1 | cut -d ',' -f 2`
@@ -21,10 +42,10 @@ mkdir -p ${out}
 
 if [[ ${interval} =~ chrM ]]
 then
-	index=chrM
+        index=chrM
 else
-	filename=${interval##*/}
-	index=${filename%-scattered.interval_list}
+        filename=${interval##*/}
+        index=${filename%-scattered.interval_list}
 fi
 
 gvcf=${out}/${tmid}_${nmid}.unfiltered.${index}.vcf.gz
@@ -36,32 +57,32 @@ echo "$(date) : Running GATK 4 Mutect2. Reference: ${ref}, Tumour: ${tmid}, Norm
 # PoN not included here
 if [[ ${index} =~ chrM ]]
 then
-	gatk --java-options "-Xmx8g -Xms8g" \
-		Mutect2 \
-		-R ${ref} \
-		-L chrM \
-		--mitochondria-mode \
-		-I ${tmbam} \
-		-I ${nmbam} \
-		-normal ${nmid} \
-		--native-pair-hmm-threads ${nt} \
-		--germline-resource ${gnomad} \
-		--f1r2-tar-gz ${f1r2} \
-		-O ${gvcf} >>${logdir}/${index}.oe 2>&1
-	echo "$(date) : Finished GATK 4 Mutect2 in mitochondrial mode for: ${gvcf}" >> ${logdir}/${index}.oe 2>&1
+        gatk --java-options "-Xmx8g" \
+                Mutect2 \
+                -R ${ref} \
+                -L chrM \
+                --mitochondria-mode \
+                -I ${tmbam} \
+                -I ${nmbam} \
+                -normal ${nmid} \
+                --native-pair-hmm-threads ${nt} \
+                --germline-resource ${gnomad} \
+                --f1r2-tar-gz ${f1r2} \
+                -O ${gvcf} >>${logdir}/${index}.oe 2>&1
+        echo "$(date) : Finished GATK 4 Mutect2 in mitochondrial mode for: ${gvcf}" >> ${logdir}/${index}.oe 2>&1
 else
-	gatk --java-options "-Xmx8g -Xms8g" \
-		Mutect2 \
-		-R ${ref} \
-		-I ${tmbam} \
-		-I ${nmbam} \
-		-normal ${nmid} \
-		--native-pair-hmm-threads ${nt} \
-		--panel-of-normals ${pon} \
-		--germline-resource ${gnomad} \
-		--f1r2-tar-gz ${f1r2} \
-		-XL chrM \
-		-L ${interval} \
-		-O ${gvcf} >>${logdir}/${index}.oe 2>&1
-	echo "$(date) : Finished GATK 4 Mutect2 for: ${gvcf}" >> ${logdir}/${index}.oe 2>&1
+        gatk --java-options "-Xmx8g" \
+                Mutect2 \
+                -R ${ref} \
+                -I ${tmbam} \
+                -I ${nmbam} \
+                -normal ${nmid} \
+                --native-pair-hmm-threads ${nt} \
+                --panel-of-normals ${pon} \
+                --germline-resource ${gnomad} \
+                --f1r2-tar-gz ${f1r2} \
+                -XL chrM \
+                -L ${interval} \
+                -O ${gvcf} >>${logdir}/${index}.oe 2>&1
+        echo "$(date) : Finished GATK 4 Mutect2 for: ${gvcf}" >> ${logdir}/${index}.oe 2>&1
 fi
