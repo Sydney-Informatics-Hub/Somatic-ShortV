4b51b923c8318ffca91767ce8ae952eb04e65a45
georgiesamaha <73086054+georgiesamaha@users.noreply.github.com>
2021-08-19 19:15:41 +1000

    Update gatk4_getpileupsummaries_make_input.sh

--- gatk4_getpileupsummaries_make_input.sh
+++ gatk4_getpileupsummaries_make_input.sh
@@ -3,21 +3,26 @@
 #########################################################
 #
 # Platform: NCI Gadi HPC
-# Description: Creates input file for gatk4_getpileupsummaries_run_parallel.pbs
 # Usage: sh gatk4_getpileupsummaries_make_input.sh /path/to/cohort.config
-# Author: Tracy Chew
-# tracy.chew@sydney.edu.au
-# Date last modified: 23/03/2021
+# Version: 1.0
 #
-# If you use this script towards a publication, please acknowledge the
-# Sydney Informatics Hub (or co-authorship, where appropriate).
+# For more details see: https://github.com/Sydney-Informatics-Hub/Somatic-ShortV
+#
+# If you use this script towards a publication, support us by citing:
+#
+# Suggest citation:
+# Sydney Informatics Hub, Core Research Facilities, University of Sydney,
+# 2021, The Sydney Informatics Hub Bioinformatics Repository, <date accessed>,
+# https://github.com/Sydney-Informatics-Hub/Germline-ShortV
+#
+# Please acknowledge the Sydney Informatics Hub and the facilities:
 #
 # Suggested acknowledgement:
-# The authors acknowledge the scientific and technical assistance
-# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
-# Hub and resources and services from the National Computational
-# Infrastructure (NCI), which is supported by the Australian Government
-# with access facilitated by the University of Sydney.
+# The authors acknowledge the technical assistance provided by the Sydney
+# Informatics Hub, a Core Research Facility of the University of Sydney
+# and the Australian BioCommons which is enabled by NCRIS via Bioplatforms
+# Australia. The authors acknowledge the use of the National Computational
+# Infrastructure (NCI) supported by the Australian Government.
 #
 #########################################################
 
